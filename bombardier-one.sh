518ab9a46f736d3972bf3bbeedcadd5e8bfb0c28
Dmitriy Beseda <dmitriy.beseda@fluxon.com>
2022-03-02 12:27:43 +0200

    urls fix + condition fix

--- bombardier-one.sh
+++ bombardier-one.sh
@@ -12,32 +12,32 @@ runningContainers=${runningContainers##*( )}
 if [[ "$containersLimit" -lt "$runningContainers" ]];
 then
   echo "$runningContainers already running. Can't add more"
-  return;
+else
+  container_name=${site##*//}
+  container_name=${container_name%/}
+  container_name=${container_name%%/*}
+  containers=$(docker ps --filter name=$container_name)
+
+  status_code=$(curl -m 2 -o /dev/null -s -w "%{http_code}" "$site")
+  status_code=$(expr "$status_code" + 0)
+
+  # If status code is zero site is already blocked
+  site_status="${site} status code ${status_code}"
+  echo "$site_status"
+  if (($status_code != 0 && $status_code <= 400)); then
+    if [[ "$containers" != *"$container_name"* ]];
+    then
+        docker run --name=$container_name -ti -d --rm alpine/bombardier -c $BOMBARDIER_CONNECTIONS -d $BOMBARDIER_TIMEOUT -l $site
+        echo "$container_name has been launched"
+        continue;
+    fi
+  elif (($status_code == 0));
+  then
+    if [[ "$containers" == *"$container_name"* ]];
+    then
+        docker kill $container_name
+        echo "$container_name has been stopped"
+    fi
+  fi
 fi
 
-container_name=${site##*//}
-container_name=${container_name%/}
-container_name=${container_name%%/*}
-containers=$(docker ps --filter name=$container_name)
-
-status_code=$(curl -m 2 -o /dev/null -s -w "%{http_code}" "$site")
-status_code=$(expr "$status_code" + 0)
-
-# If status code is zero site is already blocked
-site_status="${site} status code ${status_code}"
-echo "$site_status"
-if (($status_code != 0 && $status_code <= 400)); then
-  if [[ "$containers" != *"$container_name"* ]];
-  then
-      docker run --name=$container_name -ti -d --rm alpine/bombardier -c $BOMBARDIER_CONNECTIONS -d $BOMBARDIER_TIMEOUT -l $site
-      echo "$container_name has been launched"
-      continue;
-  fi
-elif (($status_code == 0));
-then
-  if [[ "$containers" == *"$container_name"* ]];
-  then
-      docker kill $container_name
-      echo "$container_name has been stopped"
-  fi
-fi
